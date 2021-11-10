b3bd3db83a915aca5104f867da7a7173ff6d0b06
章玉莹 <2997642893@qq.com>
2021-11-10 15:10:12 +0800

    本地项目上传码云

---
+++ Property-Management/src/test/java/cn/hd/bd/BdApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hd.bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
